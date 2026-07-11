19b645fbe3659060b1e76aa652aff62ecca4e816
Jay-23-Lebron <zhanzhande_0705@qq.com>
2026-07-11 23:43:25 +0800

    Create src/main.c with basic file comment

---
+++ src/main.c
@@ -0,0 +1,3 @@
+// C-TelephoneBook Contact Management System
+// Main entry source file
+// Personal coding portfolio for University of Adelaide CS master application, ACS assessment and Australian IT interviews
