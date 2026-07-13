01f40be381b38c272fdb160849fa708c260eddf9
Jay-23-Lebron <zhanzhande_0705@qq.com>
2026-07-13 21:09:03 +0800

    Update main.c: translate all Chinese console prompts to English

--- src/main.c
+++ src/main.c
@@ -15,31 +15,31 @@ int count=0;
 
 void addcontact(){
 	if(count>=MAX){
-		printf("通讯录已满，无法添加新联系人！\n");
+		printf("Contact list full, cannot add new contact!\n");
 		return;
 	}
 	
-	printf("请输入联系人姓名：");
+	printf("Please enter contact name:");
 	scanf("%s",contacts[count].name);
 	
-	printf("请输入联系人电话：");
+	printf("Please enter contact phone number:");
 	scanf("%s",contacts[count].phone);
 	
 	count++;
 	
-	printf("√联系人添加成功！\n");	
+	printf("√Contact added successfully!\n");	
 }
 
 void showAllcontacts(){
 	if(count==0){
-		printf("通讯录里还没有联系人，请先新建！\n");
+		printf("No contacts stored in list, please create a new contact first！\n");
 		return; 
 	}
 	
-	printf("\n==============全部联系人列表===============\n");
+	printf("\n============== All Contact List ===============\n");
 	int i; 
 	for(i=0;i<count;i++){
-		printf("序号 %d  姓名：%s  手机号：%s\n",i+1,contacts[i].name,contacts[i].phone);
+		printf("Index %d  Name：%s  Phone Number：%s\n",i+1,contacts[i].name,contacts[i].phone);
 	}
 	printf("=============================================\n");
 }
@@ -48,18 +48,18 @@ int main(){
 	while(1){
 		
 	
-	printf("================================\n");
-	printf("===========通讯录菜单===========\n");
-	printf("         1.新建联系人           \n");
-	printf("         2.删除联系人           \n");
-	printf("         3.修改联系人           \n");
-	printf("         4.查找联系人           \n");
-	printf("         5.显示所有联系人       \n");
-	printf("         0.退出通讯录           \n");
-	printf("================================\n");
+	printf("=======================================\n");
+	printf("===========Contact List Menu===========\n");
+	printf("         1.Create New Contact       \n");
+	printf("         2.Delete Contact           \n");
+	printf("         3.Edit Contact             \n");
+	printf("         4.Search Contact           \n");
+	printf("         5.Show All Contacts        \n");
+	printf("         0.Exit Contact List        \n");
+	printf("=======================================\n");
 	
 	int choice;
-	printf("请输入你的选择：");
+	printf("Please enter your option:");
 	scanf(" %d",&choice);
 	
 	
@@ -68,22 +68,22 @@ int main(){
 			addcontact();
 		    break;
 		case 2:
-		    printf("->进入删除联系人\n");
+		    printf("->Enter contact deletion page\n");
 			break;
 		case 3:
-		    printf("->进入修改联系人\n");
+		    printf("->Enter contact modification page\n");
 			break;
 		case 4:
-		    printf("->进入查找联系人\n");
+		    printf("->Enter contact search page\n");
 			break;
 		case 5:
 		    showAllcontacts();
 			break;
 		case 0:
-		    printf("再见！\n");
+		    printf("Goodbye！\n");
 			return 0; 
 		default:
-			printf("输入错误！请重新运行！\n");
+			printf("Invalid input! Please restart the program!\n");
 	}
 }
 	
